b679eca13f7b40f184dc9d36256f8c10e2ec327b
Jesus Alderete <jesusk8er@gmail.com>
2017-10-12 19:09:36 -0300

    Modify name of file in spec

--- if_work_solution.rb
+++
@@ -1,9 +0,0 @@
-variable = "false"
-otra_variable = "nil"
-
-case
-when variable == "Algún texto" then puts "No es igual a Algún texto"
-when otra_variable.nil? then puts "otra_variable es nil!"
-when variable == false then puts "Al parecer esto es lo que se debe ejecutar!"
-else puts "Nada se ejecutó? Por que?"
-end
