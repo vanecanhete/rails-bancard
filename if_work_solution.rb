b2cc11c06d4ba88b5f71ec1585fd9805e0c811bf
Jesus Alderete <jesusk8er@gmail.com>
2017-10-12 18:59:04 -0300

    Rewrite if_work with case sentence

---
+++ if_work_solution.rb
@@ -0,0 +1,9 @@
+variable = "false"
+otra_variable = "nil"
+
+case
+when variable == "Algún texto" then puts "No es igual a Algún texto"
+when otra_variable.nil? then puts "otra_variable es nil!"
+when variable == false then puts "Al parecer esto es lo que se debe ejecutar!"
+else puts "Nada se ejecutó? Por que?"
+end
