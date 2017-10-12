42c72273c1880a5d94e15b40fd37ece685838823
Daiana Sanabria <daiasana16@gmail.com>
2017-10-12 15:03:34 -0700

    commit daia

---
+++ Case.rb
@@ -0,0 +1,17 @@
+variable = "false"
+
+otra_variable = "nil"
+
+
+case 
+
+	variable == "Algún texto"
+		puts "No es igual a Algún texto"
+	otra_variable.nil?
+		puts "otra_variable es nil!"
+	variable == false
+		puts "Al parecer esto es lo que se debe ejecutar!"
+else
+
+  puts "Nada se ejecutó? Por que?"
+end
