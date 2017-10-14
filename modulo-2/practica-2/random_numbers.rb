a4aea77556cb00e421ee990d5272ce1dee02d450
Jesus Alderete <jesusk8er@gmail.com>
2017-10-14 09:33:25 -0300

    Fix random numbers to be minor than 5000

--- modulo-2/practica-2/random_numbers.rb
+++ modulo-2/practica-2/random_numbers.rb
@@ -7,10 +7,10 @@ p arr
 # Seleccionar unicamente los elementos que divido 3 den un resto = 0
 # utilizando el operador % (modulo)
 puts "Seleccion de números divisibles por 3"
-p arr.select { |element| element % 3 == 0 }
+p arr.select! { |element| element % 3 == 0 }
 
 #Utilizando el metodo 'reject' filtrar aquellos elementos menores a 5000
 #y utilzar 'sort' y 'reverse' para ordenar de forma descendente
 #Implementar todo esto en una sola linea de código
 puts "Resultado final: "
-#p arr.select { |element| element % 3 == 0 }
+p arr.reject { |element| element < 5000 }.sort.reverse
