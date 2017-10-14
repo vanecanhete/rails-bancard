283cf5234c5cae25b650e36d0a666f3cc9c3e2b7
jcroot <arrobasoft@gmail.com>
2017-10-14 09:27:21 -0300

    Merge con master - practica 1

--- modulo-2/practica-1/if_work.rb
+++ modulo-2/practica-1/if_work.rb
@@ -1,15 +1,10 @@
 variable = "false"
-otra_variable = "nil"
+otra_variable = "nill"
 
-if variable == "Algún texto"
-  puts "No es igual a Algún texto"
 
-elsif otra_variable.nil?
-  puts "otra_variable es nil!"
-
-elsif variable == false
-  puts "Al parecer esto es lo que se debe ejecutar!"
-
-else
-  puts "Nada se ejecutó? Por que?"
+case variable
+	when "Algún texto" then puts "No es igual a Algún texto"
+	when otra_variable.nil? then puts "otra variable es nil!"
+	when false then puts "Al parecer esto es lo que se debe ejecutar!"
+	else puts "Nada se ejecutó? Por que?"
 end
