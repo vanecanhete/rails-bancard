51083dd2fc3c62ef1f9ad757304148f4a06366b7
jcroot <arrobasoft@gmail.com>
2017-10-17 19:15:58 -0300

    Solucion TeamPlayers

--- modulo-2/practica-4/team_players.rb
+++ modulo-2/practica-4/team_players.rb
@@ -1,6 +1,11 @@
 class Player
-  #attr_reader
-  #initialize
+  attr_reader :name, :age, :skill_level
+  
+  def initialize(name, age, skill_level)
+    @name = name
+    @age = age
+    @skill_level = skill_level
+  end
 
   def to_s
     "<#{name}: #{skill_level}(SL), #{age}(AGE)>"
@@ -9,12 +14,26 @@ class Player
 end
 
 class Team
-  #attr_accessor
-  #initialize
-  #add_players
+  include Enumerable
+  @@teams = 0
+  attr_accessor :name, :players
+  
+  def initialize(name)
+    @name = name
+    @players = []
+    @@teams += 1
+  end
+
+  def add_players(*players)
+    @players += players
+  end
 
   def to_s
     "#{@name} team: #{@players.join(", ")}"
+  end
+
+  def each
+    @players.each { |player| yield player }
   end
 
 end
@@ -28,9 +47,10 @@ player5 = Player.new("Scott", 16, 3)
 red_team = Team.new("Red")
 red_team.add_players(player1, player2, player3, player4, player5)
 
+puts red_team.select { |p| p.age >= 14 && p.age <= 20}.reject { |p| p.skill_level < 4.5}
+
 # Seleccionar únicamente los jugadores entre 14 y 20 años y no incluir a ningún jugador con skill-level menor a 4.5
-elig_players = []#COLOCAR AQUÍ LA SELECCION#
-puts elig_players
+#puts elig_players
 # DEBE IMPRIMIR
 # => <Jim: 4.5(SL), 15(AGE)>
 # => <Joe: 5(SL), 14(AGE)>
