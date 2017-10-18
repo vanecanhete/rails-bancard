13e7d41f6bbf64d5afa131f54fed5ce4ce0c171a
paola figueredo <paofigue@gmail.com>
2017-10-18 10:12:51 -0300

    case paola

---
+++ modulo-2/practica-1/case_paola.rb
@@ -0,0 +1,16 @@
+variable = "false"
+otra_variable = "nil"
+
+case
+when variable == "Algún texto" then
+  puts "No es igual a Algún texto"
+
+when otra_variable.nil?
+  puts "otra_variable es nil osea nulo!"
+
+when variable == false
+  puts "Al parecer esto es lo que se debe ejecutar!"
+
+else
+  puts "Nada se ejecutó? Por que?"
+end
