922f8c1881b70699e20dcea8898527471d85a6a0
Vanessa Cañete <vanecanhete@gmail.com>
2017-11-04 09:52:06 -0300

    Creacion del proyecto

---
+++ modulo-4/fancy_cars/config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+FancyCars::Application.config.secret_key_base = 'e5817583617e24b1f55cad808427d466aff4ccb8d5239edf3ce6561e22f6eb259426193b715d39dcf2f9f3731d23958eeb115d7a1ca85f85d8fcf3a11300e767'
